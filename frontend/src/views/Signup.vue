95b929db1a0e778e6409906793557b46b02ccdcf
신유빈 <youbin1294@naver.com>
2020-09-09 21:35:14 +0900

    S03P22A302-40 | added | 회원가입 vue 틀 생성

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>회원가입</div>
+  <div>
+    <h1>회원가입</h1>이름 :
+    <input type="text" v-model="signupData.username" />
+    닉네임 :
+    <input type="text" v-model="signupData.nickname" />
+    아이디 :
+    <input type="email" v-model="signupData.email" />
+    비밀번호 :
+    <input type="password" v-model="signupData.password" />
+    <div>
+      <button @click="signup(signupData)">작성 완료</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   name: "Signup",
+  methods: {
+    ...mapActions(["signup"]),
+  },
+  data() {
+    return {
+      signupData: {
+        username: "",
+        nickname: "",
+        email: "",
+        password: "",
+      },
+    };
+  },
 };
 </script>
 
