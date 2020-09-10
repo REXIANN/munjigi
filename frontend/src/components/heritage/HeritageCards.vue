c5cc4c55d07a385eb932920e74458d8a61f628a9
김진혁 <gandolkim@gmail.com>
2020-09-10 12:17:53 +0900

    S03P22A302-43 | heritage components updated

--- frontend/src/components/heritage/HeritageCards.vue
+++ frontend/src/components/heritage/HeritageCards.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>This is CommunityCards.vue</div>
+  <div>
+    <div>This is CommunityCards.vue</div>
+
+    <div class="d-flex">
+      <HeritageCardItem />
+      <HeritageCardItem />
+      <HeritageCardItem />
+    </div>
+
+  </div>
 </template>
 
 <script>
+import HeritageCardItem from '@/components/heritage/HeritageCardItem'
 export default {
   name: "CommunityCards",
+  components: {
+    HeritageCardItem
+  }
 };
 </script>
 
