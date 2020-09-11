302d0a41c97d30a3ab85d5fb7c6d93e4d10bbfa3
신유빈 <youbin1294@naver.com>
2020-09-11 11:24:07 +0900

    S03P22A302-44 | updated feature | 메인페이지 틀 수정

--- frontend/src/components/home/HomeIntroduce.vue
+++ frontend/src/components/home/HomeIntroduce.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <h1>소개</h1>
+    <div>This is HomeIntroduce.vue</div>
+
     <h2>문지기란?</h2>
-    <h3 @click="goHeritage">문화재 추천</h3>사용자만을 위한 추천 서비스
-    <h3 @click="goCommunity">게시판</h3>문화재 방문 리뷰 공유
-    <h3 @click="goMap">지도</h3>문화재 위치와 주변 정보 제공
+    <div class="d-flex">
+      <h3 @click="goHeritage">문화재 추천</h3>
+      사용자만을 위한 추천 서비스
+      <h3 @click="goCommunity">게시판</h3>
+      문화재 방문 리뷰 공유
+      <h3 @click="goMap">지도</h3>
+      문화재 위치와 주변 정보 제공
+    </div>
   </div>
 </template>
 
