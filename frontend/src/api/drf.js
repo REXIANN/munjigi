110557320a19f09b06187cf9720274b9c45a3c4a
youbin-shin <youbin5575@gmail.com>
2020-09-16 15:15:53 +0900

    S03P22A302-79 | updated feature | 게시글 작성 가능

--- frontend/src/api/drf.js
+++ frontend/src/api/drf.js
@@ -1,8 +1,9 @@
 export default {
-  URL: 'http://localhost:8000',
+  URL: "http://localhost:8000",
   ROUTES: {
-    signup: '/accounts/auth/register/',
-    login: '/accounts/auth/login/',
-    logout: '/accounts/auth/logout/',
-  }
-}
+    signup: "/accounts/auth/register/",
+    login: "/accounts/auth/login/",
+    logout: "/accounts/auth/logout/",
+    review: "/review",
+  },
+};
--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -1,20 +1,60 @@
 <template>
   <div>
-      This is community.vue
-      <CommunityReviewList />
+    <div>
+      <input type="text" v-model="title" placeholder="제목을 입력해주세요" autofocus required="제목을 입력해 주세요!" />
+      <input
+        type="text"
+        v-model="content"
+        placeholder="내용을 입력해주세요"
+        autofocus
+        required="내용을 입력해 주세요!"
+        @keypress.enter="create"
+      />
+      <input type="submit" value="작성완료" @click="create" />
+    </div>
+    <button @click="getReview">불러오기</button>
+    <CommunityReviewList />
   </div>
 </template>
 
 <script>
-import CommunityReviewList from '@/components/community/CommunityReviewList'
+import SERVER from "@/api/drf";
+import axios from "axios";
+import CommunityReviewList from "@/components/community/CommunityReviewList";
+import { mapState, mapGetters, mapActions } from "vuex";
 
 export default {
   name: "Community",
   components: {
     CommunityReviewList,
-  }
-  
-}
+  },
+  created: {},
+  computed: {
+    ...mapState(["userData"]),
+    ...mapGetters(["config"]),
+  },
+  methods: {
+    create() {
+      const reviewData = {
+        title: this.title,
+        content: this.content,
+        user: this.userData.id,
+      };
+      axios.post(
+        SERVER.URL + SERVER.ROUTES.review + "/",
+        reviewData,
+        this.config
+      );
+    },
+    ...mapActions(["getReview"]),
+  },
+  data() {
+    return {
+      title: "",
+      content: "",
+    };
+  },
+};
 </script>
 
 <style type="text/css" lang="scss">
