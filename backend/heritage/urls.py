2f243de1096e128cc451868b64fd8f22f0f344ff
ketkat001 <ketkat001@gmail.com>
2020-09-22 11:50:50 +0900

    S03P23A302-19 | Heritage Bookmark has added

--- backend/heritage/urls.py
+++ backend/heritage/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import HeritageListAPI, HeritageDetailAPI, HeritageLikeAPI
+from .views import HeritageListAPI, HeritageDetailAPI, HeritageLikeAPI, HeritageBookmarkAPI
 
 urlpatterns = [
     path('', HeritageListAPI.as_view()),
     path('<int:pk>/', HeritageDetailAPI.as_view()),
     path('<int:pk>/like/', HeritageLikeAPI.as_view()),
+    path('<int:pk>/dib/', HeritageBookmarkAPI.as_view()),
 ]
