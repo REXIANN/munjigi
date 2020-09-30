4f5cc85d96f02e430a885b72b2032e5b6828c995
신유빈 <youbin1294@naver.com>
2020-09-30 21:22:22 +0900

    S03P23A302-59 | added feature | 문화재 검색 기능 구현

--- frontend/src/components/heritage/HeritageSearchBar.vue
+++ frontend/src/components/heritage/HeritageSearchBar.vue
@@ -1,24 +1,45 @@
 <template>
   <div class="heritage-search-bar">
-    <v-text-field 
-      name="input" 
-      label="찾고자 하는 문화재를 검색해 보세요!" 
-      :rules="rules" 
-      hide-details="auto"></v-text-field>
+    <v-text-field
+      name="input"
+      label="찾고자 하는 문화재를 검색해 보세요!"
+      append-icon="mdi-magnify"
+      :rules="rules"
+      v-model="searchInput"
+      hide-details="auto"
+      @keypress="searchHeritage(searchInput)"
+    ></v-text-field>
+    {{ searchHeritageList }}
   </div>
 </template>
 
 <script>
 import "@/assets/css/components/heritage/heritageSearchBar.scss";
+import SERVER from "@/api/drf";
+import axios from "axios";
 
 export default {
   name: "CommunitySearchBar",
+  methods: {
+    searchHeritage(searchInput) {
+      axios
+        .get(SERVER.URL + SERVER.ROUTES.heritage + "?query=" + searchInput)
+        .then((res) => {
+          console.log(res.data.results);
+          this.searchHeritageList = res.data.results;
+        });
+    },
+  },
   data() {
     return {
-      rules: [
-        (value) => !!value || "한 글자 이상 입력하셔야 합니다.",
-      ],
+      searchInput: "",
+      searchHeritageList: [],
+      rules: [(value) => !!value || "한 글자 이상 입력하셔야 합니다."],
     };
   },
 };
 </script>
+
+<style type="text/css" lang="scss">
+@import "@/assets/css/components/heritage/heritageSearchBar.scss";
+</style>
