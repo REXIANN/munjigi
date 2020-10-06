a351d4ddaaab601ffda9fd50d5c6470b187e77e9
최정원 <jungwon9156@khu.ac.kr>
2020-10-06 11:45:09 +0900

    S03P23A302-90

--- backend/review/serializers.py
+++ backend/review/serializers.py
@@ -8,9 +8,11 @@ class ReviewListSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format='%Y-%m-%d')
     updated_at = serializers.DateTimeField(format='%Y-%m-%d')
     users = serializers.StringRelatedField(source='user.nickname', read_only=True)
+    k_name = serializers.StringRelatedField(source='heritage.k_name', read_only=True)
+    imageurl = serializers.StringRelatedField(source='heritage.imageurl', read_only=True)
     class Meta:
         model = Review
-        fields = ('id', 'title', 'content', 'created_at', 'updated_at', 'users', 'heritage')
+        fields = ('id', 'title', 'content', 'created_at', 'updated_at', 'users', 'heritage', 'k_name', 'imageurl')
 
 
 class ReviewSerializer(serializers.ModelSerializer):
